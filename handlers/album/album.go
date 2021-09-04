04ee5b5f56b61576dd3e6135cee28e20896472dc
Aaron Cohen <acohen@mountainpasstech.com>
2021-09-04 21:03:00 +0300

    add handlers

---
+++ handlers/album/album.go
@@ -0,0 +1,21 @@
+package album
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kahunacohen/smartlyrics/models/album"
+	"net/http"
+)
+
+var as = []album.Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane"},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan"},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan"},
+}
+
+func GetAll(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, as)
+}
+func Get(c *gin.Context) {
+	//id := c.Param("id")
+	c.IndentedJSON(http.StatusOK, as[0])
+}
