4642d310ecc7ede23942b553aa635348a65e2557
Aaron Cohen <acohen@mountainpasstech.com>
2021-09-04 20:58:33 +0300

    add model test

---
+++ models/album/album_test.go
@@ -0,0 +1,13 @@
+package album
+
+import (
+	"testing"
+)
+
+func TestAlbumGet(t *testing.T) {
+	want := "Blue Train"
+	// msg, err := Hello("Gladys")
+	// if !want.MatchString(msg) || err != nil {
+	// 	t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	// }
+}
