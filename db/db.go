bc4ea580c985cf507590d4b83d658c7dc3ef91c4
Aaron Cohen <acohen@mountainpasstech.com>
2021-09-09 19:15:23 +0300

    add db conect

--- db/db.go
+++ db/db.go
@@ -10,10 +10,9 @@ import (
 func Connect() *gorm.DB {
 	dsn := "host=db user=smartlyrics password=Vp~sJT46 dbname=smartlyrics port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	fmt.Println(dsn)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	con, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Printf("%v", err)
 		panic("failed to connect database")
 	}
-	return db
+	return con
 }
